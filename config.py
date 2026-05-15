d730db03de0406a7d417f958330c50dc6b1e4987
automation-iil <automation@indianinovatix.com>
2026-05-15 14:09:41 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-SPINE_URL    = "https://inovatix.spinehrm.in"
-USERNAME     = "IIL56"
-PASSWORD     = "PassworD@1993"
-LOGIN_FOR    = "HR"
-
-MONTHS_BACK    = 0
-HEADLESS       = False
-CHROME_TIMEOUT = 30
-DATA_FILE      = "spine_attendance.json"
